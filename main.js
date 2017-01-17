b9f43632eeeeee3734494e461e47dd17b64b0f13
Samantha Davis <samantha.shea.davis@gmail.com>
2017-01-17 17:22:19 -0500

    dealer and player cardvalue and hit functions

--- main.js
+++ main.js
@@ -5,29 +5,50 @@ const cards = ['A', '2', '3', '4', '5', "6", '7', '8', '9', "10", 'J', 'Q', 'K']
 function hit() {
   let card = Math.floor(Math.random() * cards.length);
   display.innerHTML += ' ' + cards[card];
-  checkResult(false, true);
   let dealerCard = Math.floor(Math.random() * cards.length);
   dealerDisplay.innerHTML += ' ' + cards[card];
+  checkResult(false, true);
 }
 
 function checkResult(standing, hitting) {
   let currentCards = display.innerHTML.split(' ');
-  let cardValue = 0;
+  let dealerCards = dealerDisplay.innerHTML.split(' ');
+  let playerCardValue = 0;
+  let dealerCardValue = 0;
 
   currentCards.forEach(card => {
     if (parseInt(card) >= 2 && parseInt(card) <= 10) {
-      cardValue += parseInt(card);
+      playerCardValue += parseInt(card);
     } else if (card === 'J' || card == 'Q' || card === 'K') {
-      cardValue += 10;
+      playerCardValue += 10;
     } else if (card === 'A') {
-      if (cardvalue <= 10) {
-        cardValue += 11;
+      if (playerCardValue <= 10) {
+        playerCardValue += 11;
       } else {
-        cardValue += 1;
+        playerCardValue += 1;
       }
     }
   });
 
+  dealerCards.forEach(card => {
+    if (parseInt(card) >= 2 && parseInt(card) <= 10) {
+      dealerCardValue += parseInt(card);
+    } else if (card === 'J' || card == 'Q' || card === 'K') {
+      dealerCardValue += 10;
+    } else if (card === 'A') {
+      if (dealerCardValue <= 10) {
+        dealerCardValue += 11;
+      } else {
+        dealerCardValue += 1;
+      }
+    }
+  });
+console.log(playerCardValue +  '   '  + dealerCardValue);
+
+if (dealerCardValue <= 17) {
+  let dealerCard = Math.floor(Math.random() * cards.length);
+  dealerDisplay.innerHTML += ' ' + cards[card];
+}
 }
 
 document.querySelector('#hit').addEventListener('click', hit);
@@ -37,4 +58,4 @@ let card2 = Math.floor(Math.random() * cards.length);
 let dealer1 = Math.floor(Math.random() * cards.length);
 let dealer2 = Math.floor(Math.random() * cards.length);
 display.innerHTML = cards[card1] + ' ' + cards[card2];
-dealerDisplay.innerHTML = cards[dealer1] + ' ' + cards[dealer2];
+dealerDisplay.innerHTML = cards[dealer1];
